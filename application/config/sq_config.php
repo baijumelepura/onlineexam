a3065a4db2d0f38d4fbe35ef4a44443a385d3bc8
baiju <baijumca005@gmail.com>
2020-07-18 18:17:02 +0400

    test

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,5 +1,5 @@
         <?php 
-		$sq_base_url='http://localhost/savsoftquiz_v5/';
+		$sq_base_url='http://localhost/onlineexam/';
 		$sq_hostname='localhost';
 		$sq_dbname='savsoftquiz_v5.0';
 		$sq_dbusername='root';
