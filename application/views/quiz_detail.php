bf0dd3e2a9f1d46be31e1830002a07d6c4293572
vimal <vimalbabu77@gmail.com>
2020-07-24 12:31:41 +0400

    css change from vimal
    css

--- application/views/quiz_detail.php
+++ application/views/quiz_detail.php
@@ -1,126 +1,109 @@
- <div class="container">
-
-   
- <h3><?php echo $title;?></h3>
-   
- 
-
-  <div class="row">
-     <form method="post" id="quiz_detail" action="<?php echo site_url('quiz/validate_quiz/'.$quiz['quid']);?>">
-	
-<div class="col-md-12">
-<br> 
- <div class="login-panel panel panel-default">
-		<div class="panel-body"> 
-	
-	
-	
+<div class="container test-description">
+<div class="row">
+ 	<form method="post" id="quiz_detail" action="<?php echo site_url('quiz/validate_quiz/'.$quiz['quid']);?>">
+		<div class="card">
+		  <h3 class="card-header"><?php echo $title;?></h3>
 			<?php 
-		if($this->session->flashdata('message')){
+			if($this->session->flashdata('message')){
 			echo $this->session->flashdata('message');	
-		}
-		?>	
-<table class="table table-bordered">
-<tr><td><?php echo $this->lang->line('quiz_name');?></td><td><?php echo $quiz['quiz_name'];?></td></tr>
-<tr><td colspan='2'><?php echo $this->lang->line('description');?><br><?php echo $quiz['description'];?></td></tr>
-<tr><td><?php echo $this->lang->line('start_date');?></td><td><?php echo date('Y-m-d H:i:s',$quiz['start_date']);?></td></tr>
-<tr><td><?php echo $this->lang->line('end_date');?></td><td><?php echo date('Y-m-d H:i:s',$quiz['end_date']);?></td></tr>
-<tr><td><?php echo $this->lang->line('duration');?></td><td><?php echo $quiz['duration'];?></td></tr>
-<tr><td><?php echo $this->lang->line('maximum_attempts');?></td><td><?php echo $quiz['maximum_attempts'];?></td></tr>
-<tr><td><?php echo $this->lang->line('pass_percentage');?></td><td><?php echo $quiz['pass_percentage'];?></td></tr>
-<?php $ex=explode(',',$quiz['correct_score']); if(count($ex)<=1){ ?>
-<tr><td><?php echo $this->lang->line('correct_score');?></td><td><?php  echo $quiz['correct_score'];?></td></tr>
-<tr><td><?php echo $this->lang->line('incorrect_score');?></td><td><?php echo $quiz['incorrect_score'];?></td></tr>
-<?php } ?>
-<tr> <td><?php echo $this->lang->line('select_language');?></td><td> <select name="selected_lang"  id="changelang">
-<?php
-foreach($this->config->item('question_lang') as $lk => $val){
-?>
-<option value="<?php echo $lk;?>"><?php echo $val;?></option>
-<?php 
-}
-?>
-</select>
-</td></tr>
-</table>
-  
+			}
+			?>
+		  <div class="card-body">
+		    <h5 class="card-title"><?php echo $this->lang->line('quiz_name');?> : <?php echo $quiz['quiz_name'];?></h5>
+		    <p class="card-text">
+		    	<span><b><?php echo $this->lang->line('description');?></b></span> :
+				<?php echo $quiz['description'];?>
+		    	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+		    </p>
+		   <!--  <a href="#" class="btn btn-primary">Go somewhere</a> -->
+		  </div>
+		   <ul class="list-group list-group-flush">
+		   	 <li class="list-group-item">
+		   	 	<?php echo $this->lang->line('select_language');?>
+				<select name="selected_lang"  id="changelang">
+					<?php
+					foreach($this->config->item('question_lang') as $lk => $val){
+					?>
+					<option value="<?php echo $lk;?>"><?php echo $val;?></option>
+					<?php 
+					}
+					?>
+				</select>
+		   	 </li>
+		   </ul>
+		   <div class="card-body text-left">
 
-<?php 
-if($this->session->userdata('logged_in')){
-if($quiz['camera_req']==1 && $this->config->item('webcam')==true){
-?>
-<div style="color:#ff0000;"><?php echo $this->lang->line('camera_instructions');?></div>
-<div id="my_photo" style="width:500px;height:500px;background:#212121;padding:2px;border:1px solid #666666;color:red"></div>
-<br><br>
-<script type="text/javascript" src="<?php echo base_url();?>js/webcamjs/webcam.js"></script>
-	<script language="JavaScript">
-		Webcam.set({
-			width: 500,
-			height: 500,
-			image_format: 'jpeg',
-			jpeg_quality: 90
-		});
-		Webcam.attach( '#my_photo' );
+				<?php 
+				if($this->session->userdata('logged_in')){
+				if($quiz['camera_req']==1 && $this->config->item('webcam')==true){
+				?>
+				<div style="color:#ff0000;"><?php echo $this->lang->line('camera_instructions');?></div>
+				<div id="my_photo" style="width:500px;height:500px;background:#212121;padding:2px;border:1px solid #666666;color:red"></div>
+				<br><br>
+				<script type="text/javascript" src="<?php echo base_url();?>js/webcamjs/webcam.js"></script>
+					<script language="JavaScript">
+						Webcam.set({
+							width: 500,
+							height: 500,
+							image_format: 'jpeg',
+							jpeg_quality: 90
+						});
+						Webcam.attach( '#my_photo' );
 
-		
-		 function take_snapshot() {
-		     Webcam.snap( function(data_uri) {
-                document.getElementById('my_photo').innerHTML = '<img src="'+data_uri+'"/>';
-            } );
-        }
-		
-		function upload_photo(){
-		Webcam.snap( function(data_uri) {
+						
+						 function take_snapshot() {
+						     Webcam.snap( function(data_uri) {
+				                document.getElementById('my_photo').innerHTML = '<img src="'+data_uri+'"/>';
+				            } );
+				        }
+						
+						function upload_photo(){
+						Webcam.snap( function(data_uri) {
 
-    Webcam.upload( data_uri, '<?php echo site_url('quiz/upload_photo');?>',function(code, text) {
-        // Upload complete!
-        // 'code' will be the HTTP response code from the server, e.g. 200
-        // 'text' will be the raw response content
-	 document.getElementById('quiz_detail').submit();
-    });
-	});
-	
-	}
-	
-	 function capturephoto(){
-		 
-		void(take_snapshot());upload_photo(); 
-	 }
-	</script>
-	
-	<button class="btn btn-success" type="button" onClick="javascript:capturephoto();"><?php echo $this->lang->line('capture_start_quiz');?></button>
+				    Webcam.upload( data_uri, '<?php echo site_url('quiz/upload_photo');?>',function(code, text) {
+				        // Upload complete!
+				        // 'code' will be the HTTP response code from the server, e.g. 200
+				        // 'text' will be the raw response content
+					 document.getElementById('quiz_detail').submit();
+				    });
+					});
+					
+					}
+					
+					 function capturephoto(){
+						 
+						void(take_snapshot());upload_photo(); 
+					 }
+					</script>
+					
+					<button class="btn btn-success" type="button" onClick="javascript:capturephoto();"><?php echo $this->lang->line('capture_start_quiz');?></button>
 
-<?php 
-}else{
-?>	
-	<button class="btn btn-success" type="submit"><?php echo $this->lang->line('start_quiz');?></button>
- 
- <?php 
-}
-}else{
-	if($quiz['with_login']==0){ 
-	?>
-	
-	<button class="btn btn-success" type="submit"><?php echo $this->lang->line('start_quiz');?></button>
- &nbsp;&nbsp;&nbsp;&nbsp; <a href="<?php echo site_url('quiz/open_quiz/0');?>" ><?php echo $this->lang->line('back');?></a>
+				<?php 
+				}else{
+				?>	
+					<button class="btn btn-success" type="submit"><?php echo $this->lang->line('start_quiz');?></button>
+				 
+				 <?php 
+				}
+				}else{
+					if($quiz['with_login']==0){ 
+					?>
+					
+					<button class="btn btn-success" type="submit"><?php echo $this->lang->line('start_quiz');?></button>
+				 &nbsp;&nbsp;&nbsp;&nbsp; <a href="<?php echo site_url('quiz/open_quiz/0');?>" ><?php echo $this->lang->line('back');?></a>
 
-	
-	<?php 
-	}else{
-?>
-<div class="alert alert-danger"><?php echo str_replace('{base_url}',base_url(),$this->lang->line('login_required'));?></div>
-&nbsp;&nbsp;&nbsp;&nbsp; <a href="<?php echo site_url('quiz/open_quiz/0');?>" ><?php echo $this->lang->line('back');?></a>
-<?php
-	} 
-}
-?>
+					
+					<?php 
+					}else{
+				?>
+				<div class="alert alert-danger"><?php echo str_replace('{base_url}',base_url(),$this->lang->line('login_required'));?></div>
+				&nbsp;&nbsp;&nbsp;&nbsp; <a href="<?php echo site_url('quiz/open_quiz/0');?>" ><?php echo $this->lang->line('back');?></a>
+				<?php
+					} 
+				}
+				?>
+			</div> 
 		</div>
-</div>
- 
- 
- 
- 
-</div>
       </form>
 </div>
 
